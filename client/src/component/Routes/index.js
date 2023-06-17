12d94f5b6e5466c6ca61e0bad794630f1f028034
MehdiAlizadehGitHub <mehdi.alizadeh986@gmail.com>
2023-06-17 02:37:39 +0200

    .

---
+++ client/src/component/Routes/index.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route/* , Redirect */ } from 'react-router-dom';
+import React from 'react';
+import Home from '../../pages/Home';
+import Profil from '../../pages/Profil';
+import Trending from '../../pages/Trending';
+import Navbar from '../Navbar';
+
+const index = () => {
+    return (
+        <div>
+            <Router>
+                <Navbar/>
+                <Route path="/profil" exact component={Profil} />
+                <Route path="/trending" exact component={Trending} />
+                <Route path="/" exact component={Home} />
+                {/* <Redirect to="/" /> */}
+            </Router>
+        </div>
+
+    );
+};
+
+export default index;
