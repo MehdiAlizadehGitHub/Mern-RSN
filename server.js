373310e342174f3ef828fbf41486fad6654de89b
MehdiAlizadehGitHub <mehdi.alizadeh986@gmail.com>
2023-06-24 17:10:46 +0200

    .

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
+require('dotenv').config({path: './config/.env'});
+require('./config/db');
+const {checkUser, requireAuth} = require('./middleware/auth.middleware');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.static('public'))
+
+const corsOptions = {
+  origin: [process.env.CLIENT_URL, 'https://mern-rsn.onrender.com'],
+  credentials: true,
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id)
+});
+
+// routes
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+
+// server
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+})
