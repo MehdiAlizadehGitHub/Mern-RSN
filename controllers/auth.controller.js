003f435e1513b4a62ddbd1a1aad9e5fa95df1bc3
MehdiAlizadehGitHub <mehdi.alizadeh986@gmail.com>
2023-07-01 03:43:04 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -29,10 +29,11 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge });
+    res.cookie('jwt', token, { httpOnly:true , maxAge });
     res.status(200).json({ user: user._id })
   } catch (err) {
     const errors = signInErrors(err);
+    console.log({ errors })
     res.status(200).json({ errors });
   }
 }
