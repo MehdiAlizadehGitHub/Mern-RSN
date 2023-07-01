4abd6aee9b7f703d1ac49d8895635be89e014f94
MehdiAlizadehGitHub <mehdi.alizadeh986@gmail.com>
2023-07-01 05:02:41 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -29,7 +29,7 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, domain:'mern-rsn.onrender.com', maxAge });
+    res.cookie('jwt', token, { httpOnly: true, domain:'mern-rsn-api.onrender.com', maxAge, path:"/" });
     res.status(200).json({ user: user._id })
   } catch (err) {
     const errors = signInErrors(err);
